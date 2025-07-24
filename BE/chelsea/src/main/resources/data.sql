8cca772ff4f74180aca896f92147b27cedba8c56
hyunji321 <77107811+hyunji321@users.noreply.github.com>
2025-07-24 16:56:12 +0900

    chore : auth db 데이터 추가

---
+++ BE/chelsea/src/main/resources/data.sql
@@ -0,0 +1,100 @@
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025001, '김서준', 'MAJOR', NULL);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025002, '이하윤', 'NONMAJOR', 3);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025003, '박지호', 'MAJOR', 1);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025004, '최서아', 'NONMAJOR', 5);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025005, '정은우', 'MAJOR', NULL);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025006, '강지안', 'NONMAJOR', 2);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025007, '조유준', 'MAJOR', 8);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025008, '윤하은', 'NONMAJOR', 4);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025009, '장시우', 'MAJOR', NULL);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025010, '임수아', 'NONMAJOR', 7);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025011, '한도윤', 'MAJOR', 1);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025012, '오채원', 'NONMAJOR', 6);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025013, '서예준', 'MAJOR', NULL);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025014, '신유나', 'NONMAJOR', 9);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025015, '권민준', 'MAJOR', 2);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025016, '황다은', 'NONMAJOR', 5);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025017, '안주원', 'MAJOR', NULL);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025018, '송지우', 'NONMAJOR', 10);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025019, '유하준', 'MAJOR', 3);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025020, '전서윤', 'NONMAJOR', 1);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025021, '정서아', 'MAJOR', NULL);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025022, '강지안', 'NONMAJOR', 8);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025023, '최은우', 'MAJOR', 4);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025024, '김하윤', 'NONMAJOR', 2);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025025, '이시우', 'MAJOR', NULL);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025026, '박서준', 'NONMAJOR', 7);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025027, '조지호', 'MAJOR', 5);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025028, '윤수아', 'NONMAJOR', 3);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025029, '장유준', 'MAJOR', NULL);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025030, '임채원', 'NONMAJOR', 1);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025031, '한민준', 'MAJOR', 6);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025032, '오유나', 'NONMAJOR', 9);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025033, '서하준', 'MAJOR', NULL);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025034, '신지우', 'NONMAJOR', 2);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025035, '권도윤', 'MAJOR', 4);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025036, '황서윤', 'NONMAJOR', 8);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025037, '안예준', 'MAJOR', NULL);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025038, '송다은', 'NONMAJOR', 1);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025039, '유주원', 'MAJOR', 10);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025040, '전하은', 'NONMAJOR', 3);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025041, '오지호', 'MAJOR', NULL);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025042, '한서아', 'NONMAJOR', 5);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025043, '임은우', 'MAJOR', 7);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025044, '장하윤', 'NONMAJOR', 6);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025045, '윤서준', 'MAJOR', NULL);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025046, '조지안', 'NONMAJOR', 4);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025047, '강시우', 'MAJOR', 2);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025048, '정수아', 'NONMAJOR', 8);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025049, '최유준', 'MAJOR', NULL);
+INSERT INTO auth (mate_id, name, major, team_id)
+VALUES (2025050, '박채원', 'NONMAJOR', 1);
