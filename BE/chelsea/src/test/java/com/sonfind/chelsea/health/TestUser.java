046d9f7543314713ebd7c90153e59f2387e5f290
Jgone2 <jgoneit@gmail.com>
2025-07-24 14:30:40 +0900

    test: TestUser 테스트 ì용 엔티티 추구현

---
+++ BE/chelsea/src/test/java/com/sonfind/chelsea/health/TestUser.java
@@ -0,0 +1,28 @@
+package com.sonfind.chelsea.health;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "users")
+public class TestUser {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String username;
+
+	private String email;
+
+	public TestUser(String username, String email) {
+		this.username = username;
+		this.email = email;
+	}
+}
