acbd6a33eb5aec1e7167c65c9d6989bbb8b93b36
Jgone2 <jgoneit@gmail.com>
2025-07-25 14:54:32 +0900

    feat: 팀 합류 요청을 위한 TeamMateController 구현 - 로그 save섹션만 구현

--- BE/chelsea/src/main/java/com/sonfind/chelsea/controller/TeamMateController.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/controller/TeamMateController.java
@@ -1,9 +1,18 @@
-package com.sonfind.chelsea;
+package com.sonfind.chelsea.controller;
 
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sonfind.chelsea.dto.notification.NotificationRequestDto;
 import com.sonfind.chelsea.service.NotificationService;
 
 import lombok.RequiredArgsConstructor;
@@ -16,6 +25,22 @@ import lombok.extern.slf4j.Slf4j;
 public class TeamMateController {
 
 	private final NotificationService notificationService;
+	private final URI LOCATION = URI.create("/api/v1/team-mates");
 
 	@PostMapping()
+	public ResponseEntity<Map<String, Object>> createTeamMateEvent(
+		@RequestBody NotificationRequestDto notificationRequestDto
+	) {
+		log.info("createTeamMateEvent called");
+
+		try {
+			Map<String, Object> body = new HashMap<>();
+			body.put("status", "SUCCESS");
+			body.put("data", Map.of("notification", notificationService.saveNotification(notificationRequestDto)));
+			return ResponseEntity.status(HttpStatus.CREATED).header(HttpHeaders.LOCATION, LOCATION.toString()).body(body);
+		} catch (Exception e) {
+			log.error("Error creating notification: {}", e.getMessage());
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "Failed to create notification"));
+		}
+	}
 }
