d0341c281e1f903bdf7fea84c62a48a19f7e45e9
hyunji321 <77107811+hyunji321@users.noreply.github.com>
2025-07-25 09:57:43 +0900

    feat : checkstyle 적용 및 merge 템플릿 추가

--- BE/chelsea/src/main/java/com/sonfind/chelsea/controller/AuthController.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/controller/AuthController.java
@@ -11,16 +11,14 @@ import com.sonfind.chelsea.domain.auth.Auth;
 import com.sonfind.chelsea.service.AuthService;
 
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
 	private static AuthService authService;
-
-	public AuthController(AuthService authService) {
-		this.authService = authService;
-	}
 
 	@PostMapping("/sing-in")
 	public ResponseEntity<Object> singup(@RequestBody Long mateId, HttpSession session) {
@@ -31,10 +29,10 @@ public class AuthController {
 
 			return ResponseEntity.ok()
 				.header("Set-Cookie", "sessionId=" + mateId)
-				.body(mateId);
+				.build();
 		}
-
+		
 		//학번 조회가 안되는 경우 예외 처리
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not Found");
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
 }
--- BE/chelsea/src/main/java/com/sonfind/chelsea/service/AuthService.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/service/AuthService.java
@@ -2,21 +2,18 @@ package com.sonfind.chelsea.service;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sonfind.chelsea.domain.auth.Auth;
 import com.sonfind.chelsea.repository.AuthRepository;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class AuthService {
 
-	@Autowired
 	private final AuthRepository authRepository;
-
-	public AuthService(AuthRepository authRepository) {
-		this.authRepository = authRepository;
-	}
 
 	public Auth findByMateId(long mateId) {
 
