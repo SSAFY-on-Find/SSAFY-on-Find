f75dcd4343238eae43e014c5bbcc7a2176dbe91d
hyunji321 <77107811+hyunji321@users.noreply.github.com>
2025-07-29 13:24:52 +0900

    feat: 공통 db BaseEntity 구현

---
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/global/domain/BaseEntity.java
@@ -0,0 +1,37 @@
+package com.sonfind.chelsea.global.domain;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public abstract class BaseEntity {
+
+	@CreatedBy
+	private Long created_by;
+
+	@CreatedDate
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime created_at;
+
+	@LastModifiedBy
+	private Long updated_by;
+
+	@LastModifiedDate
+	@Column(nullable = true)
+	private LocalDateTime updated_at;
+}
