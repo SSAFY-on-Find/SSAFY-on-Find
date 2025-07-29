3e73673750c5d905e20c482bf4d6ca532af5338b
KIM SEONMIN <seonmiki@student.42seoul.kr>
2025-07-29 14:50:15 +0900

    feat: color, font 설정 index.css로

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <h1 className="bg-main text-text text-3xl font-bold p-4 rounded">
-      Hello world!
-    </h1>
+    <div className="bg-main">
+      <p className="text-background">Hello World</p>
+    </div>
   );
 }
 
