a6224ba9dc7a137dd82850451219144582f04a54
hyunji321 <77107811+hyunji321@users.noreply.github.com>
2025-07-29 13:22:41 +0900

    feat: code 관련 db 추가
    - 상위 main_code db 추가
    - 하위 sub_code db 추가

---
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/global/domain/SubCode.java
@@ -0,0 +1,34 @@
+package com.sonfind.chelsea.global.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubCode extends BaseEntity {
+
+	@Id
+	@Column(name = "SUB_CODE")
+	private String subCode;
+
+	@ManyToOne
+	@JoinColumn(name = "MAIN_CODE")
+	private MainCode mainCode;
+
+	@Column(name = "SUB_CODE_NAME")
+	private String subCodeName;
+
+	@Column(name = "SUB_CODE_DESCRIPTION")
+	private String subCodeDescription;
+
+	@Column(name = "USE_YN")
+	private boolean useYn;
+}
