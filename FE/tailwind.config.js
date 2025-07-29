2a65cd9886326866c90f47c04027a6867fefeb63
KIM SEONMIN <seonmiki@student.42seoul.kr>
2025-07-29 14:37:17 +0900

    error: tailwind color 적용 시도

--- FE/tailwind.config.js
+++ FE/tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+      sans: ['Pretendard', 'sans-serif'],
+      },
+      colors: {
+        main: "#6C5CE7",
+        background: "#FAFAFA",
+        text: "#1A1A1A",
+        subtext: "#A3A3A3",
+        line: "#E6E6E6",
+        error: "#FF2B37",
+        frontend: "#EA580C",
+        backend: "#0891B2",
+        fullstack: "#F59E0B",
+        embedded: "#16A34A",
+        infra: "#64748B",
+        ai: "#D946EF",
+        mobile: "#EC4899",
+      },
+    },
   },
   plugins: [],
 };
