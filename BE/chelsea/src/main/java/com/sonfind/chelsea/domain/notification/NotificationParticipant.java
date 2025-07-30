2620bfccf6cf25a7cb298585b39398b77fc293bf
Jgone2 <jgoneit@gmail.com>
2025-07-30 17:30:14 +0900

    refactor: NotificationParticipant 타입 읽음확인 관련 필드 추가

--- BE/chelsea/src/main/java/com/sonfind/chelsea/domain/notification/NotificationParticipant.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/domain/notification/NotificationParticipant.java
@@ -1,5 +1,7 @@
 package com.sonfind.chelsea.domain.notification;
 
+import java.util.Date;
+
 import com.sonfind.chelsea.types.NotificationDomainType;
 
 import lombok.AllArgsConstructor;
@@ -13,6 +15,8 @@ import lombok.Setter;
  *
  * @field id: pubId 혹은 subId
  * @field type: pubType 혹은 subType -> ["TEAM", "MATE"]
+ * @field isRead: 알림 읽음 여부
+ * @field readAt: 알림 읽음 시간(isRead가 true일 때만 값이 존재)(eg. 2023-10-01T12:00:00Z)
  * @field notificationTitle: 알람 발송 제목(eg. 김싸피님의 지원)
  * @field notificationMessage: 알람 발신 메시지(eg. 김싸피(BE, 전공)님이 팀A에 합류를 요청했습니다.)
  */
@@ -23,6 +27,12 @@ public class NotificationParticipant {
 	private final long id;
 
 	private final NotificationDomainType type;
+
+	@Setter
+	private boolean isRead;
+
+	@Setter
+	private Date readAt;
 
 	@Setter
 	private String notificationTitle;
