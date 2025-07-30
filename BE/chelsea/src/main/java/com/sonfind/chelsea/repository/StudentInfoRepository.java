2164d983e2301b39d3ad065c20be1b9508384e6a
hyunji321 <77107811+hyunji321@users.noreply.github.com>
2025-07-31 08:48:31 +0900

    feat: studentRepository 구현

---
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/repository/StudentInfoRepository.java
@@ -0,0 +1,9 @@
+package com.sonfind.chelsea.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sonfind.chelsea.domain.studentInfo.StudentInfo;
+
+public interface StudentInfoRepository extends JpaRepository<StudentInfo, Long> {
+
+}
