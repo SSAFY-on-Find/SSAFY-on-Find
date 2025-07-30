fe6d2ae56d559fa5775844226ae7e579040e4e24
Yen Seok <seokyeeun130@naver.com>
2025-07-30 09:50:39 +0900

    refactor:teamCR masterCode

--- BE/chelsea/src/main/java/com/sonfind/chelsea/global/domain/BaseEntity.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/global/domain/BaseEntity.java
@@ -6,8 +6,10 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,6 +21,7 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
 	@CreatedBy
