19796d42e3d0e6532569cc0b85a00364972dd9a9
Jgone2 <jgoneit@gmail.com>
2025-07-31 07:59:02 +0900

    refactor: 컬렉션 변경으로 인한 repository 분리 작업

--- BE/chelsea/src/main/java/com/sonfind/chelsea/repository/NotificationStatusRepository.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/repository/NotificationStatusRepository.java
@@ -1,4 +1,13 @@
 package com.sonfind.chelsea.repository;
 
-public interface NotificationStatusRepository {
+import com.sonfind.chelsea.domain.notification.NotificationStatusDocument;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sonfind.chelsea.types.RecipientRole;
+import com.sonfind.chelsea.types.NotificationStatus;
+
+@Repository
+public interface NotificationStatusRepository extends MongoRepository<NotificationStatusDocument, Long> {
+
 }
