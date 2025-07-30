5f5e18ef7c82730f762356426af287111fd47b84
hyunji321 <77107811+hyunji321@users.noreply.github.com>
2025-07-31 08:52:05 +0900

    feat: 자기소개 등록 dto 구현

--- BE/chelsea/src/main/java/com/sonfind/chelsea/dto/studentInfo/StudentInfoCreateRequestDto.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/dto/studentInfo/StudentInfoCreateRequestDto.java
@@ -1,23 +1,20 @@
-package com.sonfind.chelsea.dto.mates;
+package com.sonfind.chelsea.dto.studentInfo;
 
 import java.util.List;
 
 import org.hibernate.validator.constraints.Length;
-import org.springframework.web.multipart.MultipartFile;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public record MateCreateRequestDto(
+public record StudentInfoCreateRequestDto(
 	@NotBlank String track,
 	@NotBlank String position,
 	@NotBlank String goal,
 	String description,
 	String mbti,
 	@NotNull List<@NotBlank String> techStack,
-	@Size(max = 3) List<@NotBlank @Length(max = 5) String> strength,
-	MultipartFile profile,
-	MultipartFile portfolio
+	@Size(max = 3) List<@NotBlank @Length(max = 5) String> strength
 ) {
 }
