b39b0a6e8ce28da0cd94d682794c3c81122d8639
hyunji321 <77107811+hyunji321@users.noreply.github.com>
2025-07-31 08:51:07 +0900

    feat: swagger MultipartFile 인식을 위한 converter 추가
    - swagger에서 @RequestPart로 MultipartFile에 공백 입력 시 `MediaType.APPLICATION_OCTET_STREAM` 오류 해결을 위한 converter

---
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/global/convert/MultipartJackson2HttpMessageConverter.java
@@ -0,0 +1,31 @@
+package com.sonfind.chelsea.global.convert;
+
+import java.lang.reflect.Type;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Component
+public class MultipartJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
+	protected MultipartJackson2HttpMessageConverter(ObjectMapper objectMapper) {
+		super(objectMapper, MediaType.APPLICATION_OCTET_STREAM);
+	}
+
+	@Override
+	public boolean canWrite(Type type, Class<?> clazz, MediaType mediaType) {
+		return super.canWrite(type, clazz, mediaType);
+	}
+
+	@Override
+	public boolean canWrite(Class<?> clazz, MediaType mediaType) {
+		return super.canWrite(clazz, mediaType);
+	}
+
+	@Override
+	protected boolean canWrite(MediaType mediaType) {
+		return super.canWrite(mediaType);
+	}
+}
