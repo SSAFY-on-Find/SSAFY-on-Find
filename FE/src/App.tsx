18034217c1c29fa91f28132ca8af99989a7cb242
JeongYeonKim <kimjeongyeon113@gmail.com>
2025-07-30 15:54:07 +0900

    chore:포맷팅

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,19 +1,10 @@
-import { Route,Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 
 import Dashboard from "@/pages/DashboardPage"
 import MyTeam from "@/pages/MyTeamPage"
 
 import "@/index.css"
 
-const test1 = ["자바", "비전공"]
-const test2 = [
-	["프론트", "POS001"],
-	["백엔드", "POS001"],
-	["풀스택", "POS001"],
-	["임베디드", "POS001"],
-	["모바일", "POS001"],
-	["AI", "POS001"],
-]
 function App() {
 	return (
 		<Routes>
