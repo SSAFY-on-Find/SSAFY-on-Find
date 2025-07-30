19b7dd47f702095496e33de74456be12896709fe
박의균 <pugpug9483@gmail.com>
2025-07-30 09:08:21 +0900

    feat : 헬스체크 컨트롤러 생성

--- BE/chelsea/src/main/java/com/sonfind/chelsea/global/config/WebSecurityConfig.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/global/config/WebSecurityConfig.java
@@ -5,28 +5,30 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-
 public class WebSecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-
-		httpSecurity.authorizeHttpRequests((authorize) -> authorize
-			.requestMatchers("/api/v1/auth/**").permitAll()
-			.requestMatchers(HttpMethod.GET, "/api/v1/mates").permitAll()
-			.requestMatchers("/api/v1/mates/events").permitAll()
-			.requestMatchers(HttpMethod.GET, "/api/v1/teams").permitAll()
-			.requestMatchers(
-				"/swagger-ui/**",
-				"/swagger-ui.html",
-				"/v3/api-docs/**",
-				"/webjars/**"
-			).permitAll()
-			.anyRequest().authenticated());
+		httpSecurity
+			.csrf(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests((authorize) -> authorize
+				.requestMatchers("/api/v1/auth/**").permitAll()
+				.requestMatchers(HttpMethod.GET, "/health-check").permitAll()
+				.requestMatchers(HttpMethod.GET, "/api/v1/mates").permitAll()
+				.requestMatchers("/api/v1/mates/events").permitAll()
+				.requestMatchers(HttpMethod.GET, "/api/v1/teams").permitAll()
+				.requestMatchers(
+					"/swagger-ui/**",
+					"/swagger-ui.html",
+					"/v3/api-docs/**",
+					"/webjars/**"
+				).permitAll()
+				.anyRequest().authenticated());
 
 		return httpSecurity.build();
 	}
