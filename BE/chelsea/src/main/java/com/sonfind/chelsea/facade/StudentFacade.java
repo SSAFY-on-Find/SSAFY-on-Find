bf1c6e450ceabe9088d2d1a30bc5d75229a1e4c3
Jgone2 <jgoneit@gmail.com>
2025-07-31 14:16:11 +0900

    feat: 알림에서 Student 및 StudentInfo 서비스 레이어 참조를 위한 Facade 클래스 구현

--- BE/chelsea/src/main/java/com/sonfind/chelsea/facade/StudentFacade.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/facade/StudentFacade.java
@@ -1,4 +1,47 @@
 package com.sonfind.chelsea.facade;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sonfind.chelsea.domain.student.Students;
+import com.sonfind.chelsea.dto.student.StudentForNotificationResponseDto;
+import com.sonfind.chelsea.dto.student.StudentUnionForNotificationResponseDto;
+import com.sonfind.chelsea.dto.studentInfo.StudentInfoForNotificationResponseDto;
+import com.sonfind.chelsea.service.StudentInfoService;
+import com.sonfind.chelsea.service.StudentService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class StudentFacade {
+	private final StudentService studentService;
+	private final StudentInfoService studentInfoService;
+
+	// 학생 ID로 학생 정보를 조회하는 메소드
+	public Students findByStudentId(long studentId) {
+		return studentService.findByStudentId(studentId);
+	}
+
+	// 팀 ID로 학생 정보 리스트를 조회하는 메소드
+	public List<Students> findAllByTeamId(Long teamId) {
+		return studentService.findAllByTeamId(teamId);
+	}
+
+	// 학생 ID로 학생 정보를 조회하는 메소드(SSE용)
+	public StudentUnionForNotificationResponseDto findByStudentIdForSse(Long studentId) {
+		StudentForNotificationResponseDto findStudent = studentService.findByStudentIdForSse(studentId);
+		StudentInfoForNotificationResponseDto findStudentInfo = studentInfoService.findByStudentId(
+			findStudent.studentId());
+
+		return StudentUnionForNotificationResponseDto.builder()
+			.studentId(findStudent.studentId())
+			.name(findStudent.name())
+			.isMajor(findStudent.isMajor())
+			.position(findStudentInfo.position())
+			.track(findStudentInfo.track())
+			.profileImageUrl(findStudentInfo.profileImageUrl())
+			.build();
+	}
 }
