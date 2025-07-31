b8d39717c395b38458cda424b7d1c661d2467d99
Jgone2 <jgoneit@gmail.com>
2025-07-31 10:44:23 +0900

    feat: pending 알림 ê로그 관련 find 추가

--- BE/chelsea/src/main/java/com/sonfind/chelsea/repository/NotificationRepository.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/repository/NotificationRepository.java
@@ -8,21 +8,11 @@ import com.sonfind.chelsea.domain.notification.NotificationDocument;
 
 @Repository
 public interface NotificationRepository extends MongoRepository<NotificationDocument, Long> {
-    /**
-    * publisherId, publisherType, subscriberId, subscriberType 조건으로
-    * 가장 최신 문서를 가져옵니다.
-    */
-    @Query(value = "{ "
-          + "'publisherId': ?0, "
-          + "'publisherType': ?1, "
-          + "'subscriberId': ?2, "
-          + "'subscriberType': ?3"
-          + "}",
-          sort  = "{ 'createdAt': -1 }")
-    NotificationDocument findLatest(
-          Long publisherId,
-          String publisherType,
-          Long subscriberId,
-          String subscriberType
-    );
+	/**
+	 * publisherId, publisherType, subscriberId, subscriberType 조건으로
+	 * 가장 최신 문서를 가져옵니다.
+	 */
+	@Query(value = "{ " + "'publisherId': ?0, " + "'publisherType': ?1, " + "'subscriberId': ?2, "
+		+ "'subscriberType': ?3" + "}", sort = "{ 'updatedAt': -1 }")
+	NotificationDocument findLatest(Long publisherId, String publisherType, Long subscriberId, String subscriberType);
 }
