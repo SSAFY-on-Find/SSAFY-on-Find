8533fc9d6d2731bc9fcf5a75c4d9a8b51e2323ad
박의균 <pugpug9483@gmail.com>
2025-07-31 16:44:27 +0900

    chore : git에서 docker compose관리하기

--- INFRA/deploy.sh
+++ INFRA/deploy.sh
@@ -3,11 +3,13 @@
 export BE_DOCKER_IMAGE=$1
 export FE_DOCKER_IMAGE=$2
 
+COMPOSE_FILE="docker-compose.prod.yml"
+
+echo "Deploying with compose file: ${COMPOSE_FILE}"
 echo "Deploying Backend Image: ${BE_DOCKER_IMAGE}"
 echo "Deploying Frontend Image: ${FE_DOCKER_IMAGE}"
 
-docker-compose pull backend frontend
-
-docker-compose up -d --force-recreate backend frontend
+docker-compose -f ${COMPOSE_FILE} pull backend frontend
+docker-compose -f ${COMPOSE_FILE} up -d --force-recreate backend frontend
 
 echo "Deployment completed."
