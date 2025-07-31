9f9c6125b3406528bdddf216e479c8d2aea3c863
Jgone2 <jgoneit@gmail.com>
2025-07-31 10:45:22 +0900

    refactor: 컬렉션 분리로 인한 로직 수정(진행 중)

--- BE/chelsea/src/main/java/com/sonfind/chelsea/service/NotificationService.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/service/NotificationService.java
@@ -3,15 +3,19 @@ package com.sonfind.chelsea.service;
 import java.util.Date;
 
 import org.apache.coyote.BadRequestException;
+import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.sonfind.chelsea.domain.notification.NotificationDocument;
-import com.sonfind.chelsea.domain.notification.NotificationParticipant;
+import com.sonfind.chelsea.domain.notification.NotificationStatusDocument;
 import com.sonfind.chelsea.dto.notification.NotificationRequestDto;
 import com.sonfind.chelsea.repository.NotificationRepository;
+import com.sonfind.chelsea.repository.NotificationStatusRepository;
 import com.sonfind.chelsea.types.NotificationDomainType;
 import com.sonfind.chelsea.types.NotificationStatus;
+import com.sonfind.chelsea.types.NotificationType;
+import com.sonfind.chelsea.types.RecipientRole;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,77 +25,99 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class NotificationService {
 
-	private final NotificationRepository notificationRepository;
+	private final NotificationRepository notificationRepo;
+	private final NotificationStatusRepository statusRepo;
 
 	/**
-	 * 알림을 저장합니다.
+	 * 알림을 저장
 	 * 알림 발신자와 수신자의 정보를 NotificationParticipant 객체로 생성하고,
-	 * NotificationDocument 객체를 생성하여 MongoDB에 저장합니다.
-	 * @param dto
+	 * NotificationDocument 객체를 생성하여 MongoDB에 저장
+	 * @param dto: NotificationRequestDto
 	 */
 	public NotificationDocument saveNotification(NotificationRequestDto dto) throws BadRequestException {
-		// dto에서 발신자와 수신자의 정보를 추출하여 NotificationParticipant 객체를 생성.
-		NotificationParticipant pub = new NotificationParticipant(dto.getPubId(),
-			NotificationDomainType.from(dto.getPubType()));
-		NotificationParticipant sub = new NotificationParticipant(dto.getSubId(),
-			NotificationDomainType.from(dto.getSubType()));
+		// String → Enum 변환 (대소문자 구분이 있다면 toUpperCase() 등으로 맞춰주세요)
+		NotificationType typeEnum =
+			NotificationType.valueOf(dto.getType().toUpperCase());
+		NotificationDomainType pubTypeEnum =
+			NotificationDomainType.valueOf(dto.getPubType().toUpperCase());
+		NotificationDomainType subTypeEnum =
+			NotificationDomainType.valueOf(dto.getSubType().toUpperCase());
 
-		if (notificationRepository.existsByPublisherIdAndPublisherTypeAndSubscriberIdAndSubscriberTypeAndStatus(
-			pub.getId(), pub.getType(), sub.getId(), sub.getType(), NotificationStatus.PENDING)) {
-			log.info("Notification already exists");
-			throw new BadRequestException("Notification already exists! code=" + HttpStatus.BAD_REQUEST);
+		Date now = new Date();
+		// 이벤트 발행 시점의 현재 날짜를 가져옴
+		Date currentDate = getCurrentDate();
+
+		// 알림 발신자와 수신자의 정보를 NotificationParticipant 객체로 생성
+		NotificationDocument findNotificationLog = findLatestNotification(dto);
+		// 해당 알림 중 PENDING 상태의 알림이 있는지 확인
+		hasPendingStatus(findNotificationLog.getId());
+
+		// NotificationDocument 객체를 생성
+		NotificationDocument notificationLog = NotificationDocument.builder()
+			.type(typeEnum)
+			.publisherId(dto.getPubId())
+			.publisherType(pubTypeEnum)
+			.subscriberId(dto.getSubId())
+			.subscriberType(subTypeEnum)
+			.createdAt(now)
+			.updatedAt(now)
+			.build();
+
+		// 알림을 저장
+		NotificationDocument savedNotification = notificationRepo.save(notificationLog);
+		log.info("알림이 저장되었습니다: {}", savedNotification.getId());
+
+		// 알림 상태를 PENDING으로 저장
+		NotificationStatusDocument pub = NotificationStatusDocument.builder()
+			.notificationId(savedNotification.getId())
+			.targetId(savedNotification.getPublisherId())
+			.role(RecipientRole.PUBLISHER)
+			.status(NotificationStatus.PENDING)
+			.isRead(true)
+			.readAt(currentDate)
+			.notificationTitle("dto.getNotificationTitle()")
+			.notificationMessage("dto.getNotificationMessage()")
+			.createdAt(currentDate)
+			.updatedAt(currentDate)
+			.build();
+	}
+
+	/**
+	 * 알림을 저장하기 전에, 가장 최근에 업데이트된 알림을 찾음
+	 * @param dto
+	 * @return NotificationDocument
+	 * @throws BadRequestException
+	 */
+	private NotificationDocument findLatestNotification(NotificationRequestDto dto) throws BadRequestException {
+		// 가장 최근에 업데이트된 알림을 찾음
+		NotificationDocument lastUpdatedLog = notificationRepo.findLatest(
+			dto.getPubId(),
+			dto.getPubType(),
+			dto.getSubId(),
+			dto.getSubType()
+		);
+
+		if (lastUpdatedLog == null) {
+			throw new BadRequestException("HttpStatus: " + HttpStatus.BAD_REQUEST + " | 알림을 찾을 수 없습니다.");
 		}
 
-		/**
-		 * TODO: 알림 발신자와 수신자 구분 방식 고려
-		 * - 1. TEAM_TEAM: 팀 합치기 제안
-		 * - 2. STUDENT_TEAM: 팀에 지원
-		 * - 3. TEAM_STUDENT: 팀에 초대
-		 * 와 같은 ENUM을 하나 더 생성하여 분기 고려 중입니다.
-		 * 이 부분은 추후에 개선할 예정입니다. 혹시 더 좋은 의견이 있으시면 코드리뷰로 남겨주시면 감사하겠습니다
-		 */
-		// title과 메시지를 설정
-		if (pub.getType() == NotificationDomainType.TEAM && sub.getType() == NotificationDomainType.TEAM) {
-			// 발신자
-			pub.setNotificationTitle("OOO" + "에 합치기 제안");
-			pub.setNotificationMessage("OOO" + "에 팀 합치기를 제안했습니다.");
-			// 수신자
-			sub.setNotificationTitle("OOO" + "의 합치기 제안");
-			sub.setNotificationMessage("OOO" + "에서 팀 합치기를 제안했습니다.");
-		} else if (pub.getType() == NotificationDomainType.STUDENT && sub.getType() == NotificationDomainType.TEAM) {
-			pub.setNotificationTitle("OOO" + "에 지원");
-			pub.setNotificationMessage("OOO" + "에 지원했습니다.");
+		return lastUpdatedLog;
+	}
 
-			sub.setNotificationTitle("OOO" + "님의 지원");
-			sub.setNotificationMessage("OOO" + "님이 지원했습니다.");
-		} else if (pub.getType() == NotificationDomainType.TEAM && sub.getType() == NotificationDomainType.STUDENT) {
-			pub.setNotificationTitle("OOO" + "님에게 초대");
-			pub.setNotificationMessage("OOO" + "님을 팀에 초대했습니다.");
-
-			sub.setNotificationTitle("OOO" + "의 초대");
-			sub.setNotificationMessage("OOO" + "에서 초대했습니다.");
-		} else {
-			log.error("Invalid notification type combination: pubType={}, subType={}", pub.getType(), sub.getType());
-			throw new BadRequestException("Invalid notification type combination! code=" + HttpStatus.BAD_REQUEST);
-		}
-		log.info("Creating notification: pub={}, sub={}", pub, sub);
-
-		try {
-			Date now = getCurrentDate();
-			// NotificationDocument 객체를 생성하고 MongoDB에 저장
-			NotificationDocument notification = NotificationDocument.builder()
-				.publisher(pub)
-				.subscriber(sub)
-				.status(NotificationStatus.PENDING)
-				.createdAt(now)
-				.updatedAt(now)
-				.build();
-			NotificationDocument savedNotification = notificationRepository.save(notification);
-			log.info("Notification saved successfully: {}", savedNotification);
-			return savedNotification;
-		} catch (Exception e) {
-			log.error("Failed to save notification: {}", e.getMessage());
-			throw new BadRequestException("Failed to save notification! code=" + HttpStatus.INTERNAL_SERVER_ERROR);
+	/**
+	 * 알림이 이미 존재하는지 확인합니다.
+	 * 알림이 존재하는 경우, 해당 알림의 상태가 PENDING인지 확인
+	 * PENDING 상태인 경우, 알림을 저장하지 않고 false를 반환
+	 * @param notificationId
+	 * @return boolean
+	 * @throws BadRequestException
+	 */
+	private void hasPendingStatus(ObjectId notificationId) throws BadRequestException {
+		Boolean hasPending = statusRepo.existsByNotificationIdAndStatus(notificationId,
+			NotificationStatus.PENDING);
+		if (hasPending) {
+			log.info("완료 처리되지 않은 알림이 이미 존재하며, 상태가 PENDING입니다. 알림을 저장하지 않습니다.");
+			throw new BadRequestException("HttpStatus: " + HttpStatus.BAD_REQUEST + " | 알림이 이미 존재하며, 상태가 PENDING입니다.");
 		}
 	}
 
