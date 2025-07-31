b6b3877639f621be1126b3437822f27277abed7e
KIM SEONMIN <seonmiki@student.42seoul.kr>
2025-07-31 13:50:27 +0900

    refactor: nav -> molecules로 컴포넌트화

--- FE/src/layout/SideBar.tsx
+++ FE/src/layout/SideBar.tsx
@@ -1,41 +1,10 @@
-import { NavLink } from "react-router-dom"
-import { ChartColumnIncreasing, Notebook, Settings,User, Users } from 'lucide-react';
-
-interface INavProps {
-	navIcon: React.ElementType,
-  navTitle: string,
-  to: string,
-}
-
-function NavBtn({ navIcon: Icon, navTitle, to }: INavProps) {
-	return (
-		<NavLink
-      to={to}
-      className={({ isActive }) =>
-        `flex items-center gap-2 px-4 py-2 rounded-md font-medium transition-colors ${
-          isActive
-            ? "bg-main text-background"
-            : "text-subtext hover:bg-main/10"
-        }`
-      }
-    >
-      <Icon className="w-5 h-5" />
-      <div className="text-sm">{navTitle}</div>
-    </NavLink>
-    )
-}
+import { Nav } from "@/components/molecules";
 
 function SideBar() {
 	return (
     <aside className="fixed top-[64px] left-0 h-[calc(100vh-64px)] w-[200px] bg-background flex flex-col">
-      <nav className="flex flex-col gap-2 px-2 py-4">
-        <NavBtn navIcon={ChartColumnIncreasing} navTitle="통계 대시보드" to="/" />
-        <NavBtn navIcon={Notebook} navTitle="내 팀 관리" to="/myteam"/>
-        <NavBtn navIcon={Users} navTitle="팀 목록" to="/teamlist"/>
-        <NavBtn navIcon={User} navTitle="교육생 목록" to="/studentlist"/>
-        <NavBtn navIcon={Settings} navTitle="마이 페이지" to="/myprofile"/>
-      </nav>
-        <div>채팅</div>
+      <Nav />
+      <div> 채팅 목록</div>
     </aside>
 	)
 }
