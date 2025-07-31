ca10639d53aac3306c5219062feb21925d42e353
hyunji321 <77107811+hyunji321@users.noreply.github.com>
2025-07-31 16:06:11 +0900

    feat: 자기소개 조회 dto 구현

---
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/dto/studentInfo/StudentInfoResponseDto.java
@@ -0,0 +1,29 @@
+package com.sonfind.chelsea.dto.studentInfo;
+
+import java.util.List;
+
+import com.sonfind.chelsea.domain.studentInfo.UploadedFile;
+import com.sonfind.chelsea.dto.student.StudentResponse;
+import com.sonfind.chelsea.dto.subcode.SubCodeResponse;
+import com.sonfind.chelsea.dto.teams.TeamResponse;
+
+import lombok.Builder;
+
+@Builder
+public record StudentInfoResponseDto(
+
+	StudentResponse student,
+
+	SubCodeResponse position,
+	SubCodeResponse track,
+	SubCodeResponse goal,
+	SubCodeResponse mbti,
+	List<SubCodeResponse> techStack,
+	List<String> strength,
+	String description,
+	String profileImageUrl,
+	UploadedFile portfolio,
+
+	TeamResponse teamInfo
+) {
+}
