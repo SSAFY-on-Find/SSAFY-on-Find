8f365328b0a2ecbbdabd3c2c2f3fca2604e26a1b
조현지 <chji9936@naver.com>
2025-07-31 09:34:57 +0900

    feat: @Repository 어노테이션 추가

--- BE/chelsea/src/main/java/com/sonfind/chelsea/repository/MainCodeRepository.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/repository/MainCodeRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sonfind.chelsea.global.domain.MainCode;
 
+@Repository
 public interface MainCodeRepository extends JpaRepository<MainCode, String> {
 }
--- BE/chelsea/src/main/java/com/sonfind/chelsea/repository/StudentInfoRepository.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/repository/StudentInfoRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sonfind.chelsea.domain.studentInfo.StudentInfo;
 
+@Repository
 public interface StudentInfoRepository extends JpaRepository<StudentInfo, Long> {
 
 }
