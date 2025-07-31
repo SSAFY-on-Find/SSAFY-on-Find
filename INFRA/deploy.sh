3ce6025a86f049c7e55454149dd18b242c094ec2
박의균 <pugpug9483@gmail.com>
2025-07-31 16:34:56 +0900

    fix : 특정 이미지만 pull로 변경

--- INFRA/deploy.sh
+++ INFRA/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 export BE_DOCKER_IMAGE=$1
-export FE_DOCKER_IMAGE=$2 
+export FE_DOCKER_IMAGE=$2
 
 echo "Deploying Backend Image: ${BE_DOCKER_IMAGE}"
 echo "Deploying Frontend Image: ${FE_DOCKER_IMAGE}"
 
-docker-compose pull
+docker-compose pull backend frontend
 
 docker-compose up -d --force-recreate backend frontend
 
