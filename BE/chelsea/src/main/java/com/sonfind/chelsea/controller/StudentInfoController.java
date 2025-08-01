814122827dc597444e09a0610e79042d6359ae41
hyunji321 <77107811+hyunji321@users.noreply.github.com>
2025-08-01 15:34:11 +0900

    feat: 자기소개 수정 api 구현

--- BE/chelsea/src/main/java/com/sonfind/chelsea/controller/StudentInfoController.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/controller/StudentInfoController.java
@@ -9,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -17,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.sonfind.chelsea.dto.studentInfo.StudentInfoCreateRequestDto;
 import com.sonfind.chelsea.dto.studentInfo.StudentInfoResponseDto;
+import com.sonfind.chelsea.dto.studentInfo.StudentInfoUpdateRequestDto;
 import com.sonfind.chelsea.service.StudentInfoService;
 
 import io.swagger.v3.oas.annotations.Parameter;
@@ -61,6 +63,21 @@ public class StudentInfoController {
 		body.put("data", studentInfo);
 
 		return ResponseEntity.ok().body(body);
+	}
+
+	@PatchMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<Map<String, Object>> updateStudentInfo(
+		@Parameter(content = @Content(mediaType = "application/json"))
+		@Valid @RequestPart StudentInfoUpdateRequestDto requestDto,
+
+		@RequestPart(value = "profile", required = false) MultipartFile profile,
+		@RequestPart(value = "portfolio", required = false) MultipartFile portfolio,
+		@CookieValue("sessionId") Long studentId
+	) throws IOException {
+
+		studentInfoService.updateStudentInfo(studentId, requestDto, profile, portfolio);
+
+		return null;
 	}
 
 }
