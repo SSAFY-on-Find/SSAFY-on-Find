f33ad82a8e4da7adc9b65e43c3d06eda26564c99
Jgone2 <jgoneit@gmail.com>
2025-08-01 13:55:04 +0900

    feat: Repository Annotation 추가

--- BE/chelsea/src/main/java/com/sonfind/chelsea/repository/TeamRepository.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/repository/TeamRepository.java
@@ -1,9 +1,11 @@
 package com.sonfind.chelsea.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.sonfind.chelsea.domain.teams.Team;
 
+@Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
 	boolean existsByName(String name);
 }
