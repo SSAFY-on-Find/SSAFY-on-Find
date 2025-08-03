257d09493640f69ab88a9028972f6bf8a27a928b
Jgone2 <jgoneit@gmail.com>
2025-08-04 05:53:21 +0900

    refactor: mongoDb PK경우 Long -> ObjectId 자료형으로 변경 및 알림 조회위한 메서드 추가

--- BE/chelsea/src/main/java/com/sonfind/chelsea/repository/NotificationStatusRepository.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/repository/NotificationStatusRepository.java
@@ -2,6 +2,8 @@ package com.sonfind.chelsea.repository;
 
 import java.util.List;
 
+import com.sonfind.chelsea.types.NotificationDomainType;
+import com.sonfind.chelsea.types.RecipientRole;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -10,9 +12,12 @@ import com.sonfind.chelsea.domain.notification.NotificationStatusDocument;
 import com.sonfind.chelsea.types.NotificationStatus;
 
 @Repository
-public interface NotificationStatusRepository extends MongoRepository<NotificationStatusDocument, Long> {
+public interface NotificationStatusRepository extends MongoRepository<NotificationStatusDocument, ObjectId> {
 
 	List<NotificationStatusDocument> findByNotificationId(ObjectId notificationLogId);
 
 	Boolean existsByNotificationIdAndStatus(ObjectId notificationId, NotificationStatus status);
+
+	List<NotificationStatusDocument> findAllByTargetIdAndTargetTypeAndRole(Long studentId, NotificationDomainType targetType, RecipientRole role);
+	int countByTargetIdAndTargetTypeAndRoleAndIsReadFalse(Long studentId, NotificationDomainType targetType, RecipientRole role);
 }
