3adedd26526204bc7cc22dbf764ee049a30c0b85
JeongYeonKim <kimjeongyeon113@gmail.com>
2025-08-03 10:41:26 +0900

    feat: 검색창 컴포넌트 개발 완료

--- FE/src/components/atoms/SearchBar.tsx
+++ FE/src/components/atoms/SearchBar.tsx
@@ -1,13 +1,32 @@
+import { useState } from "react"
 import { Search } from "lucide-react"
 
 function SearchBar() {
+  const [searchValue, setSearchValue] = useState("")
+  const handleSearch = () => {
+    if (searchValue.trim() === "") {
+      alert("내용을 입력하세요") // toast가 준비되면 바꾸면 좋을 것 같아요!!
+      return
+    }
+    console.log("검색버튼 클릭! 내용 : ", searchValue) // 검색 로직 개발 시 여기다 추가하기!!
+  }
+  const handleEnderPress = (ele: { key: string }) => {
+    if (ele.key === "Enter") {
+      handleSearch()
+    }
+  }
+
   return (
-    <>
-      <div className="flex">
-        <Search />
-        <input className="border-subtext/30 w-full rounded-full border-2" placeholder="이름으로 검색..."></input>
-      </div>
-    </>
+    <div className="relative">
+      <Search className="text-subtext absolute top-1/2 left-3 h-5 w-5 -translate-y-1/2 transform" />
+      <input
+        className="border-subtext/30 placeholder:text-subtext h-[45px] w-full rounded-full border-1 py-2 pr-4 pl-9 text-sm"
+        placeholder="이름으로 검색..."
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+        onKeyDown={handleEnderPress}
+      ></input>
+    </div>
   )
 }
 export default SearchBar
