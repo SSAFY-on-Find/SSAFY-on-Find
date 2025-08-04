e7aded51ce5106f3f974308d3943f8e4fc7ba8d9
Yen Seok <seokyeeun130@naver.com>
2025-08-04 14:04:29 +0900

    feat:내팀상세조회와 타팀상세조회

--- BE/chelsea/src/main/java/com/sonfind/chelsea/domain/teams/Team.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/domain/teams/Team.java
@@ -40,8 +40,7 @@ public class Team extends BaseEntity {
 	@Column(nullable = false)
 	private String name;
 
-	//varchar(80)
-	@Column(nullable = false)
+	@Column(nullable = false, length = 80)
 	private String description;
 
 	@ManyToOne
