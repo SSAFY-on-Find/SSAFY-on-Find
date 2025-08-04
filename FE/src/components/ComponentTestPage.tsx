1c99fc187d9a7fe70fad9c36a3dad2562066290e
JeongYeonKim <kimjeongyeon113@gmail.com>
2025-08-04 17:30:59 +0900

    feat: 유저 검색 모달 추가
    -검색창 실시간 검색
    - 클릭시 student id 기반으로 onclick이벤트 발생

--- FE/src/components/ComponentTestPage.tsx
+++ FE/src/components/ComponentTestPage.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 import { CheckTag, InputBox, MainTag, MajorTag, NormalTag, PositionTag, SearchBar, WhiteTag } from "./atoms"
-import { ConfirmModal, TeamDetailModal } from "./templates"
+import { ConfirmModal, StudentSearchModal, TeamDetailModal } from "./templates"
 
 // API로 기술스택 70여개를 받아온다고 가정하고 상수에 담아두었어요
 const TECH_STACKS = [
@@ -14,7 +14,7 @@ const TECH_STACKS = [
   { id: "tech007", name: "MySQL" },
   { id: "tech008", name: "AWS" },
 ]
-// ComponentTestPage.tsx에 추가
+// 팀 세부 정보 모달 테스트용 MockData 입니다
 const sampleTeamData = {
   id: "team001",
   name: "팀 001",
@@ -63,7 +63,24 @@ const sampleTeamData = {
     },
   ],
 }
-
+// 교육생 검색 모달 테스트용 MockData 입니다
+const students = [
+  { id: "std001", name: "김싸피", major: "비전공", position: "임베디드", hasTeam: true },
+  { id: "std002", name: "이싸", major: "전공", position: "풀스텍", hasTeam: false },
+  { id: "std003", name: "박싸피", major: "비전공", position: "백엔드", hasTeam: true },
+  { id: "std004", name: "조싸피", major: "전공", position: "모바일", hasTeam: true },
+  { id: "std005", name: "이싸피", major: "비전공", position: "임베디드", hasTeam: false },
+  { id: "std006", name: "김박싸피", major: "비전공", position: "임베디드", hasTeam: true },
+  { id: "std007", name: "이싸", major: "전공", position: "풀스텍", hasTeam: false },
+  { id: "std008", name: "박싸피", major: "비전공", position: "백엔드", hasTeam: true },
+  { id: "std009", name: "조싸피", major: "전공", position: "모바일", hasTeam: true },
+  { id: "std010", name: "이싸피", major: "비전공", position: "임베디드", hasTeam: false },
+  { id: "std011", name: "김싸피", major: "비전공", position: "임베디드", hasTeam: true },
+  { id: "std012", name: "이싸", major: "전공", position: "풀스텍", hasTeam: false },
+  { id: "std013", name: "박싸피", major: "비전공", position: "백엔드", hasTeam: true },
+  { id: "std014", name: "조싸피", major: "전공", position: "모바일", hasTeam: true },
+  { id: "std015", name: "이싸피", major: "비전공", position: "임베디드", hasTeam: false },
+]
 export default function ComponentTestPage() {
   // 1. CheckTag 사용법
   // 기술스택이 선택되면 2개 이상일 경우 관리하기 위하여 배열로 선언.(기술스택 ID를 저장할 예정입니다.)
@@ -96,6 +113,7 @@ export default function ComponentTestPage() {
   const [ConfirmModal_inviteAccept, setConfirmModal_inviteAccept] = useState(false)
   const [teamDetailModal, setTeamDetailModal] = useState(false)
   const [selectedTeam, setSelectedTeam] = useState(sampleTeamData)
+  const [studentSearchModal, setStudentSearchModal] = useState(false)
 
   const handleOut = () => {
     setConfirmModal_teamout(false)
@@ -156,6 +174,13 @@ export default function ComponentTestPage() {
         >
           팀 상세 정보
         </button>
+
+        <button
+          className="rounded-md bg-amber-600 px-4 py-2 text-white hover:bg-amber-800"
+          onClick={() => setStudentSearchModal(true)}
+        >
+          교육생 찾기 모달
+        </button>
       </div>
       <ConfirmModal
         isOpen={ConfirmModal_teamout}
@@ -175,6 +200,11 @@ export default function ComponentTestPage() {
         confirmText="수락"
       />
       <TeamDetailModal isOpen={teamDetailModal} onClose={() => setTeamDetailModal(false)} teamData={selectedTeam} />
+      <StudentSearchModal
+        isOpen={studentSearchModal}
+        onClose={() => setStudentSearchModal(false)}
+        students={students}
+      />
       <div className="">
         <p>small - 채팅입력이나 기본 입력</p>
         <InputBox
---
+++ FE/src/components/templates/StudentSearchModal.tsx
@@ -0,0 +1,79 @@
+import { useEffect, useState } from "react"
+import { Send } from "lucide-react"
+
+import { MajorTag, PositionTag, SearchBar, UserImg } from "../atoms"
+
+import Modal from "./Modal"
+interface IStudentSearchModal {
+  isOpen: boolean
+  onClose: () => void
+  students: IChatUser[]
+  onStudentClick: (userId: string) => void
+}
+interface IChatUser {
+  id: string
+  name: string
+  major: string
+  position: string
+  hasTeam: boolean
+}
+interface IStudentListItem extends IChatUser {
+  onClick: () => void
+}
+function StudentListItem({ id, name, major, position, hasTeam, onClick }: IStudentListItem) {
+  return (
+    <div
+      className="hover:bg-main/10 flex w-full cursor-pointer items-center justify-between gap-2 rounded-md px-1 py-1"
+      onClick={onClick}
+    >
+      <div className="flex items-center gap-2">
+        <UserImg name={name} size={"s"} showTeamBadge={true} hasTeam={hasTeam} />
+        <div className="text-sm font-medium">{name}</div>
+        <div className="flex items-center justify-center gap-1">
+          <MajorTag tagContent={major} />
+          <PositionTag positionName={position} />
+        </div>
+      </div>
+      <Send className="text-main hover:cursor-pointer" />
+    </div>
+  )
+}
+
+function StudentSearchModal({ isOpen, onClose, students, onStudentClick }: IStudentSearchModal) {
+  const [searchQuery, setSearchQuery] = useState("")
+
+  useEffect(() => {
+    if (isOpen) {
+      setSearchQuery("")
+    }
+  }, [isOpen])
+
+  const filteredStudents = students.filter((ele) => ele.name.toLowerCase().includes(searchQuery.toLowerCase()))
+  const handleSearch = (query: string) => {
+    setSearchQuery(query)
+  }
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} size={"m"}>
+      <div className="min-h-[550px] p-10">
+        <div className="px-[10px] py-[10px]">
+          <h3 className="text-text mb-[15px] text-xl font-bold">교육생 찾기</h3>
+          <SearchBar onSearch={handleSearch} />
+        </div>
+        <div className="flex max-h-[370px] flex-col gap-3 overflow-y-auto px-[15px] pt-[10px]">
+          {filteredStudents.map((ele) => (
+            <StudentListItem
+              key={ele.id}
+              id={ele.id}
+              name={ele.name}
+              major={ele.major}
+              position={ele.position}
+              hasTeam={ele.hasTeam}
+              onClick={() => onStudentClick(ele.id)}
+            />
+          ))}
+        </div>
+      </div>
+    </Modal>
+  )
+}
+export default StudentSearchModal
