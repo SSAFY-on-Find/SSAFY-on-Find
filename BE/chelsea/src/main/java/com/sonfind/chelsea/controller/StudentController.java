17798090ad6c1b6fc6adbf42068ae2a895222a6c
hyunji321 <77107811+hyunji321@users.noreply.github.com>
2025-08-04 13:09:03 +0900

    feat: 교육생 목록 조회 api 구현

--- BE/chelsea/src/main/java/com/sonfind/chelsea/controller/StudentController.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/controller/StudentController.java
@@ -1,14 +1,21 @@
 package com.sonfind.chelsea.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sonfind.chelsea.domain.student.Students;
+import com.sonfind.chelsea.dto.student.StudentListResponseDto;
 import com.sonfind.chelsea.service.StudentService;
 
 import jakarta.servlet.http.HttpSession;
@@ -36,5 +43,18 @@ public class StudentController {
 
 		//학번 조회가 안되는 경우 예외 처리
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	}
+
+	@GetMapping
+	public ResponseEntity<Map<String, Object>> getStudentList(@CookieValue("sessionId") Long studentId) {
+
+		List<StudentListResponseDto> data = studentService.getStudentList(studentId);
+
+		Map<String, Object> body = new HashMap<String, Object>();
+
+		body.put("status", "SUCCESS");
+		body.put("data", data);
+
+		return ResponseEntity.ok().body(body);
 	}
 }
