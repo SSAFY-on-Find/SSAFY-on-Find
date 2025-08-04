02af8ff3cd6df31c76810c8ed95b599fd4c224a8
Jgone2 <jgoneit@gmail.com>
2025-08-04 17:17:42 +0900

    feat: NotificaionStatus를 targetId 및 role, ㄴstatus로 필터링하여 조í회가능한 메서드 구현

--- BE/chelsea/src/main/java/com/sonfind/chelsea/repository/NotificationStatusRepository.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/repository/NotificationStatusRepository.java
@@ -2,14 +2,14 @@ package com.sonfind.chelsea.repository;
 
 import java.util.List;
 
-import com.sonfind.chelsea.types.NotificationDomainType;
-import com.sonfind.chelsea.types.RecipientRole;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.sonfind.chelsea.domain.notification.NotificationStatusDocument;
+import com.sonfind.chelsea.types.NotificationDomainType;
 import com.sonfind.chelsea.types.NotificationStatus;
+import com.sonfind.chelsea.types.RecipientRole;
 
 @Repository
 public interface NotificationStatusRepository extends MongoRepository<NotificationStatusDocument, ObjectId> {
@@ -18,6 +18,12 @@ public interface NotificationStatusRepository extends MongoRepository<Notificati
 
 	Boolean existsByNotificationIdAndStatus(ObjectId notificationId, NotificationStatus status);
 
-	List<NotificationStatusDocument> findAllByTargetIdAndTargetTypeAndRole(Long studentId, NotificationDomainType targetType, RecipientRole role);
-	int countByTargetIdAndTargetTypeAndRoleAndIsReadFalse(Long studentId, NotificationDomainType targetType, RecipientRole role);
+	List<NotificationStatusDocument> findAllByTargetIdAndTargetTypeAndRole(Long studentId,
+		NotificationDomainType targetType, RecipientRole role);
+
+	int countByTargetIdAndTargetTypeAndRoleAndIsReadFalse(Long studentId, NotificationDomainType targetType,
+		RecipientRole role);
+
+	NotificationStatusDocument findByNotificationIdAndTargetIdAndRoleAndStatus(ObjectId notificationId,
+		Long targetId, RecipientRole role, NotificationStatus status);
 }
