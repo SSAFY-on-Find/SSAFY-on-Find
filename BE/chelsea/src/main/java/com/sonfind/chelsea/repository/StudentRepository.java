ad2a73f33e0d562641ecb07cfb0309c6e121469a
hyunji321 <77107811+hyunji321@users.noreply.github.com>
2025-08-04 13:06:41 +0900

    feat: 교육생 목록 조회 query문 구현

--- BE/chelsea/src/main/java/com/sonfind/chelsea/repository/StudentRepository.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/repository/StudentRepository.java
@@ -4,13 +4,30 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.sonfind.chelsea.domain.student.Students;
+import com.sonfind.chelsea.dto.student.StudentListQueryDto;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Students, Long> {
 	Optional<Students> findByStudentId(Long studentId);
 
 	List<Students> findAllByTeamId(Long teamId);
+
+	@Query("SELECT new com.sonfind.chelsea.dto.student.StudentListQueryDto(" +
+		"s.studentId, s.name, s.majorYn, " +
+		"pos.subCode, pos.subCodeName, " +
+		"track.subCode, track.subCodeName, " +
+		"goal.subCode, goal.subCodeName, " +
+		"si.profileImageUrl, " +
+		"t.name) " +
+		"FROM StudentInfo si " +
+		"RIGHT JOIN si.student s " +
+		"LEFT JOIN Team t ON t.teamId = s.teamId " +
+		"LEFT JOIN si.positionCode pos " +
+		"LEFT JOIN si.trackCode track " +
+		"LEFT JOIN si.goalCode goal")
+	List<StudentListQueryDto> findStudentList();
 }
