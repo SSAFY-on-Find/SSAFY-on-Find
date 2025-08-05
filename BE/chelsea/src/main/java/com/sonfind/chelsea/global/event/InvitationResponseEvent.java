e52daa1a74a1e8317b71d8f391c47a5de3c7efa8
Jgone2 <jgoneit@gmail.com>
2025-08-05 16:14:13 +0900

    feat: Pending 이후 수락/거절/취소 이벤트 요청시 사용할 이벤트 요소들에 대한 RequestDto

--- BE/chelsea/src/main/java/com/sonfind/chelsea/global/event/InvitationResponseEvent.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/global/event/InvitationResponseEvent.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.bson.types.ObjectId;
 import org.springframework.context.ApplicationEvent;
 
+import com.sonfind.chelsea.types.NotificationDomainType;
 import com.sonfind.chelsea.types.NotificationStatus;
 
 import lombok.Getter;
@@ -12,17 +13,29 @@ import lombok.Getter;
 @Getter
 public class InvitationResponseEvent extends ApplicationEvent {
 	private final ObjectId notificationId;
+	private final long pubId;
+	private final NotificationDomainType pubType;
+	private final long subId;
+	private final NotificationDomainType subType;
 	private final NotificationStatus status;
 	private final Date updatedAt;
 
 	public InvitationResponseEvent(
 		Object source,
 		ObjectId notificationId,
+		long pubId,
+		NotificationDomainType pubType,
+		long subId,
+		NotificationDomainType subType,
 		NotificationStatus status,
 		Date updatedAt
 	) {
 		super(source);
 		this.notificationId = notificationId;
+		this.pubId = pubId;
+		this.pubType = pubType;
+		this.subId = subId;
+		this.subType = subType;
 		this.status = status;
 		this.updatedAt = updatedAt;
 	}
@@ -30,9 +43,13 @@ public class InvitationResponseEvent extends ApplicationEvent {
 	public static InvitationResponseEvent of(
 		Object source,
 		ObjectId notificationId,
+		long pubId,
+		NotificationDomainType pubType,
+		long subId,
+		NotificationDomainType subType,
 		NotificationStatus status,
 		Date updatedAt
 	) {
-		return new InvitationResponseEvent(source, notificationId, status, updatedAt);
+		return new InvitationResponseEvent(source, notificationId, pubId, pubType, subId, subType, status, updatedAt);
 	}
 }
