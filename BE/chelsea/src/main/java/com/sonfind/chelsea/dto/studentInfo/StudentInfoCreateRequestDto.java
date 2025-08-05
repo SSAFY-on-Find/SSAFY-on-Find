aac7a651d14e6e69dc73f375ba95c429cd72062c
hyunji321 <77107811+hyunji321@users.noreply.github.com>
2025-08-05 15:19:20 +0900

    docs: swagger API 문서화를 위한 어노테이션 추가

--- BE/chelsea/src/main/java/com/sonfind/chelsea/dto/studentInfo/StudentInfoCreateRequestDto.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/dto/studentInfo/StudentInfoCreateRequestDto.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.hibernate.validator.constraints.Length;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+@Schema(description = "자기소개 작성 요청 DTO")
 public record StudentInfoCreateRequestDto(
 	@NotBlank String track,
 	@NotBlank String position,
