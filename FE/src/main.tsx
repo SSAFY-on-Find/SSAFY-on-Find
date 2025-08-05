7675fcd67c3af0935fefb9bbf6762aa2da9a14ec
JeongYeonKim <kimjeongyeon113@gmail.com>
2025-08-05 17:35:29 +0900

    feat: react query devtools 추가 및 진입점에 react query 추기

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -1,15 +1,28 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 import App from "./App.tsx"
 
 import "./index.css"
-
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 10 * 60 * 1000,
+      retry: 2,
+      refetchOnWindowFocus: true,
+    },
+  },
+})
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 )
