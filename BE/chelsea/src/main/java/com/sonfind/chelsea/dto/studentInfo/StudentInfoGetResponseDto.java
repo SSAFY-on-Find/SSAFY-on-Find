7ed70b9740fca9247e6c813b2e95fb777d2388b1
조현지 <chji9936@naver.com>
2025-08-05 16:15:02 +0900

    fix: dto 내 @schema 어노테이션 삭제

--- BE/chelsea/src/main/java/com/sonfind/chelsea/dto/studentInfo/StudentInfoGetResponseDto.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/dto/studentInfo/StudentInfoGetResponseDto.java
@@ -7,44 +7,23 @@ import com.sonfind.chelsea.dto.student.StudentResponseDto;
 import com.sonfind.chelsea.dto.subcode.SubCodeResponseDto;
 import com.sonfind.chelsea.dto.teams.TeamSimpleResponseDto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
-@Schema(title = "자기소개 조회 response DTO")
 public record StudentInfoGetResponseDto(
 
-	@Schema(description = "학생 정보")
 	StudentResponseDto student,
 
-	@Schema(description = "포지션 정보")
 	SubCodeResponseDto position,
-
-	@Schema(description = "트랙 정보")
 	SubCodeResponseDto track,
-
-	@Schema(description = "목표 정보")
 	SubCodeResponseDto goal,
-
-	@Schema(description = "mbti 정보")
 	SubCodeResponseDto mbti,
-
-	@Schema(description = "테크 정보")
 	List<SubCodeResponseDto> techStack,
-
-	@Schema(description = "강점 정보")
 	List<String> strength,
-
-	@Schema(description = "자기소개 설명")
 	String description,
-
-	@Schema(description = "프로필 이미지 정보")
 	String profileImageUrl,
-
-	@Schema(description = "포트폴리오 ")
 	UploadedFile portfolio,
-
-	@Schema(description = "팀 정보")
+	
 	TeamSimpleResponseDto teamInfo
 ) {
 }
--- BE/chelsea/src/main/java/com/sonfind/chelsea/dto/studentInfo/StudentInfoUpdateRequestDto.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/dto/studentInfo/StudentInfoUpdateRequestDto.java
@@ -4,12 +4,10 @@ import java.util.List;
 
 import org.hibernate.validator.constraints.Length;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-@Schema(description = "자기소개 수정 요청 DTO")
 public record StudentInfoUpdateRequestDto(
 	@NotBlank String track,
 	@NotBlank String position,
