ddfa0a83643ab12ca6ee127c309f9989288cefc7
Jgone2 <jgoneit@gmail.com>
2025-08-06 14:53:43 +0900

    refactor: 배포환경 추가

--- BE/chelsea/src/main/java/com/sonfind/chelsea/global/config/CorsConfig.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/global/config/CorsConfig.java
@@ -18,7 +18,10 @@ public class CorsConfig {
 
 		corsConfiguration.setAllowCredentials(true);
 
-		corsConfiguration.setAllowedOriginPatterns(Arrays.asList("http://localhost:*"));
+		corsConfiguration.setAllowedOriginPatterns(Arrays.asList(
+			"http://localhost:*",
+			"https://i13a704.p.ssafy.io/api/v1"
+		));
 
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 		corsConfiguration.addExposedHeader("Authorization");
