d00459f330b96285133ec6e2e03fc08d9f56b3bd
hyunji321 <77107811+hyunji321@users.noreply.github.com>
2025-08-07 15:12:15 +0900

    feat: 트랙별 전공자, 포지션 별 교육생 수 조회 query문 구현

--- BE/chelsea/src/main/java/com/sonfind/chelsea/repository/StudentInfoRepository.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/repository/StudentInfoRepository.java
@@ -6,12 +6,9 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.sonfind.chelsea.domain.student.Student;
 import com.sonfind.chelsea.domain.studentInfo.StudentInfo;
-import com.sonfind.chelsea.dto.studentInfo.response.StudentInfoGetQueryDto;
 
 @Repository
 public interface StudentInfoRepository extends JpaRepository<StudentInfo, Long> {
@@ -21,42 +18,23 @@ public interface StudentInfoRepository extends JpaRepository<StudentInfo, Long> 
 	@EntityGraph(attributePaths = "student.classCode")
 	Optional<StudentInfo> findByStudent_StudentId(Long studentId);
 
-	@Query("SELECT new com.sonfind.chelsea.dto.studentInfo.response.StudentInfoGetQueryDto("
-		+ " s.studentId, s.name, s.majorYn, "
-		+ " pos.subCode, pos.subCodeName, "
-		+ "track.subCode, track.subCodeName, "
-		+ "goal.subCode, goal.subCodeName, "
-		+ "mbti.subCode, mbti.subCodeName, "
-		+ "si.techStack, si.strength, si.description, "
-		+ "si.profileImageUrl, si.portfolio, "
-		+ "t.teamId,t.name, "
-		+ "COALESCE(t.majorCount,0), COALESCE(t.nonMajorCount,0) , "
-		+ "teamTrack.subCodeName )"
-		+ "FROM StudentInfo si "
-		+ "JOIN si.student s "
-		+ "LEFT JOIN Team t ON t.teamId = s.teamId "
-		+ "LEFT JOIN t.track teamTrack "
-		+ "LEFT JOIN si.positionCode pos "
-		+ "LEFT JOIN si.trackCode track "
-		+ "LEFT JOIN si.goalCode goal "
-		+ "LEFT JOIN si.mbtiCode mbti "
-		+ "WHERE si.student.studentId = :studentId")
-	StudentInfoGetQueryDto findStudentInfoByStudentId(@Param("studentId") Long studentId);
-
-	@Query("""
-		SELECT si FROM StudentInfo si 
-		LEFT JOIN FETCH si.positionCode 
-		WHERE si.student.studentId IN :studentIds
-		""")
-	List<StudentInfo> findByStudent_StudentIdIn(@Param("studentIds") List<Long> studentIds);
-
-	@Query("""
-		SELECT si 
-		FROM StudentInfo si 
-		LEFT JOIN FETCH si.positionCode 
-		WHERE si.student.studentId IN :studentIds
-		""")
-	List<StudentInfo> findBasicInfoByStudentIds(@Param("studentIds") List<Long> studentIds);
-
-	Long student(Student student);
+	@Query(value = """
+			SELECT 
+				sc_track.sub_code_name AS track_name,
+				sc_pos.sub_code_name AS position_name,
+				CASE 
+					WHEN s.major_yn = TRUE THEN '전공'
+					ELSE '비전공'
+				END AS major_type,
+				COUNT(si.id) AS student_count
+			FROM student_info si
+			LEFT JOIN student s ON si.student_id = s.student_id
+			LEFT JOIN sub_code sc_track ON si.track_code = sc_track.sub_code
+			LEFT JOIN sub_code sc_pos ON si.position_code = sc_pos.sub_code
+			GROUP BY
+					track_name,position_name, major_type
+			ORDER BY 
+					track_name, position_name,major_type, student_count;
+		""", nativeQuery = true)
+	List<Object[]> getTrackPositionMajorRatio();
 }
