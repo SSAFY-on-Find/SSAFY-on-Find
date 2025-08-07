0858f47e396f47fabd7e6651e96cd82d1e4af5b6
박의균 <pugpug9483@gmail.com>
2025-08-07 17:03:48 +0900

    fix : cors 오류 해결

--- BE/chelsea/src/main/java/com/sonfind/chelsea/global/config/CorsConfig.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/global/config/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
 
 		corsConfiguration.setAllowedOriginPatterns(Arrays.asList(
 			"http://localhost:*",
-			"https://i13a704.p.ssafy.io/api/v1"
+			"https://i13a704.p.ssafy.io"
 		));
 
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
