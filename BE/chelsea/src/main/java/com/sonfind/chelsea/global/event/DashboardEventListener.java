5da963aa11935138a62e54cbeea42f4acff0ea48
Jgone2 <jgoneit@gmail.com>
2025-08-08 15:20:51 +0900

    feat: 대시보드 ë©메시지 전송을 위한 이벤트 리스너및  및 양식 구현

--- BE/chelsea/src/main/java/com/sonfind/chelsea/global/event/DashboardEventListener.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/global/event/DashboardEventListener.java
@@ -1,4 +1,18 @@
 package com.sonfind.chelsea.global.event;
 
+import com.sonfind.chelsea.service.SseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class DashboardEventListener {
+
+	private final SseService sseService;
+
+	@EventListener
+	public void onDashboardEvent() {
+
+	}
 }
--- BE/chelsea/src/main/java/com/sonfind/chelsea/global/event/DashboardMessage.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/global/event/DashboardMessage.java
@@ -1,4 +1,11 @@
 package com.sonfind.chelsea.global.event;
 
-public record DashboardMessage() {
+import com.sonfind.chelsea.types.DashboardEventType;
+import lombok.Builder;
+
+@Builder
+public record DashboardMessage<T>(
+		DashboardEventType eventType,
+		T data
+) {
 }
