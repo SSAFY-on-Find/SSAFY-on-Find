f2db565150dd39ff195dee7a3ad5093a88b20117
Jgone2 <jgoneit@gmail.com>
2025-08-08 16:41:03 +0900

    feat: 대시보드 이벤트 생성 서비스 및 조회 관련 서비스 구현

--- BE/chelsea/src/main/java/com/sonfind/chelsea/service/DashBoardQueryService.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/service/DashBoardQueryService.java
@@ -16,12 +16,22 @@ public class DashBoardQueryService {
 	private final StudentService studentService;
 
 	/**
+	 * 희망 트랙별 통계 정보를 조회합니다.
+	 */
+//	public Map<String, TrackPositionMajorRatioResponseDto> computeForTrackPositionMajorRatio() {
+//		log.info("희망 트랙별 통계 정보를 조회합니다.");
+////		return studentService.getTrackPositionMajorRatio();
+//	}
+
+
+	/**
 	 * 전체 팀빌딩 진행률을 계산합니다.
 	 * 팀에 속한 인원 수와 전공 여부를 기반으로 진행률을 계산합니다.
 	 *
 	 * @return TeamProgressDto 객체로 팀빌딩 진행률, 비전공자 수, 전공자 수를 포함합니다.
 	 */
 	public TeamProgressDto computeForTeamProgress() {
+		log.info("전체 팀빌딩 진행률을 계산합니다.");
 		Map<String, TeamRatioDto> teamRatio = studentService.getTeamRatio();
 
 		String nonMajor = "비전공";
