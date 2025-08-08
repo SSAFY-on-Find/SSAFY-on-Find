e6a89fcd9b010d2f88cc43a743508ebb78c4b273
hyunji321 <77107811+hyunji321@users.noreply.github.com>
2025-08-08 13:38:16 +0900

    feat: WebSecurityConfig에 session관련 설정 추가
    - maximunSession(1) : 계정 당 하나의 session 값만 유효하게 설정
    - maxSessionPreventLogin(false) : 다른 session 로그인을 했을 때 기존 로그인 session 만료
    - sessionFixation : sessionId 값이 변경해도 cookie 값은 변경하지 않도록 설정

--- BE/chelsea/src/main/java/com/sonfind/chelsea/controller/StudentController.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/controller/StudentController.java
@@ -55,10 +55,6 @@ public class StudentController {
 
 		StudentSignInResponseDto student = studentService.signIn(request);
 
-		HttpSession oldSession = httpServletRequest.getSession(false);
-		if (oldSession != null) {
-			oldSession.invalidate();
-		}
 		HttpSession newSession = httpServletRequest.getSession(true);
 		newSession.setAttribute("loginUser", student.studentId());
 		Map<String, Object> body = new HashMap<>();
@@ -80,6 +76,7 @@ public class StudentController {
 					example = "{\"status\": \"FAIL\", \"message\": \"작성한 자기소개서가 없습니다.\"}"
 				)))
 	})
+
 	@GetMapping
 	public ResponseEntity<Map<String, Object>> getStudentList(
 		@Parameter(hidden = true)
@@ -91,8 +88,9 @@ public class StudentController {
 		Map<String, Object> body = new HashMap<String, Object>();
 
 		body.put("status", "SUCCESS");
-		body.put("data", data);
+		body.put("data", Map.of("students", data));
 
 		return ResponseEntity.ok().body(body);
 	}
+
 }
