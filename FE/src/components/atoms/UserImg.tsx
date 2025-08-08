628c4ec5740d47582335de3ebb60e788c3837011
KIM SEONMIN <seonmiki@student.42seoul.kr>
2025-08-08 16:19:16 +0900

    feat: UserImg에 url props 추가 및 수정페이지 프로필 미리보기

--- FE/src/components/atoms/UserImg.tsx
+++ FE/src/components/atoms/UserImg.tsx
@@ -5,6 +5,7 @@ interface IUserImg {
   hasTeam?: boolean
   showTeamBadge: boolean
   isEdit?: boolean
+  url?: string
 }
 
 const sizeMap = {
@@ -23,15 +24,14 @@ const badgeSizeMap = {
   xl: "w-[32px] h-[32px] -right-[2px] -bottom-[2px]",
 }
 
-function UserImg({ name, size, hasTeam = false, showTeamBadge, isEdit = false }: IUserImg) {
+function UserImg({ name, size, hasTeam = false, showTeamBadge, isEdit = false, url = "" }: IUserImg) {
   const sizeClass = sizeMap[size] || sizeMap.m
   const badgeClass = badgeSizeMap[size] || badgeSizeMap.m
 
   return (
-    // <div className={`${sizeClass} aspect-square border border-line rounded-full overflow-hidden`}>
     <div className={`relative ${sizeClass} border-line aspect-square flex-shrink-0 rounded-full border`}>
       <img
-        src={`https://api.dicebear.com/9.x/notionists-neutral/svg?seed=${name}`}
+        src={url === "" ? `https://api.dicebear.com/9.x/notionists-neutral/svg?seed=${name}` : url}
         className="h-full w-full rounded-full object-cover"
         alt={`${name} 프로필 이미지`}
       />
--- FE/src/pages/EditProfilePage/index.tsx
+++ FE/src/pages/EditProfilePage/index.tsx
@@ -27,8 +27,19 @@ export default function ProfileEditPage() {
   const { data: codes, isLoading: isCodesLoading, error: codesError } = useProfileCodes()
   const { data: infos, isLoading: isInfosLoading, error: infosError } = useGetProfile()
   const { user } = useUserStore()
-  const { position, track, techStack, goal, strength, portfolio, portfolioFile, description, setCodes } =
-    useProfileStore()
+  const {
+    position,
+    track,
+    techStack,
+    goal,
+    strength,
+    portfolio,
+    portfolioFile,
+    profileImageUrl,
+    profileImageFile,
+    description,
+    setCodes,
+  } = useProfileStore()
   const [activeMarkdownTab, setActiveMarkdownTab] = useState<"left" | "right">("left")
   const [strengthInput, setStrengthInput] = useState("")
   const [isEditingMbti, setIsEditingMbti] = useState(false)
@@ -52,6 +63,7 @@ export default function ProfileEditPage() {
         description: infos.description,
         profileImageFile: null,
         portfolioFile: null,
+        profileImageUrl: infos.profileImageUrl,
       })
       if (infos.mbti?.subcodeName) {
         setIsEditingMbti(true)
@@ -139,8 +151,9 @@ export default function ProfileEditPage() {
     setCodes({ mbti: null })
   }
 
-  if (isCodesLoading) return <div>로딩 중...</div>
+  if (isCodesLoading || isInfosLoading) return <div>로딩 중...</div>
   if (codesError || !codes) return <div>코드 리스트를 불러올 수 없습니다.</div>
+  if (infosError || !infos) return <div>저장된 자기소개 정보를 불러올 수 없습니다.</div>
 
   return (
     <div className="bg-background flex min-h-screen flex-col gap-7 px-15 py-10">
@@ -220,7 +233,13 @@ export default function ProfileEditPage() {
               style={{ display: "none" }}
               onChange={handleProfileImgChange}
             />
-            <UserImg name={user?.name} size={"xl"} showTeamBadge={false} isEdit={true} />
+            <UserImg
+              name={user?.name}
+              size={"xl"}
+              showTeamBadge={false}
+              isEdit={true}
+              url={profileImageFile ? URL.createObjectURL(profileImageFile) : profileImageUrl}
+            />
           </div>
         </div>
         <div className="flex flex-col gap-1">
