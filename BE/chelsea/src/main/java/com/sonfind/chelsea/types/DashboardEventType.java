73899dd394dac7fae8345edba7a66decbc757fb7
Jgone2 <jgoneit@gmail.com>
2025-08-08 15:21:42 +0900

    feat: 대시보드 이벤트 관련 enum관리

--- BE/chelsea/src/main/java/com/sonfind/chelsea/types/DashboardEventType.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/types/DashboardEventType.java
@@ -1,4 +1,19 @@
 package com.sonfind.chelsea.types;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum DashboardEventType {
+	TEAM_MEMBER_CHANGED("team_member_changed"), // 팀원 합류 / 퇴장
+	TEAM_PROGRESS_UPDATED("team_progress_updated"), // 팀 빌딩 진행률
+	TEAM_UPDATED("team_updated"), // 팀 정보 업데이트
+	STUDENT_INFO_UPDATED("student_info_updated"); // 학생 정보 업데이트(희망 트랙별 통계)
+
+	private final String value;
+
+	public String getValue() {
+		return value;
+	}
 }
--- BE/chelsea/src/main/java/com/sonfind/chelsea/types/MemberChageAction.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/types/MemberChageAction.java
@@ -1,4 +1,17 @@
 package com.sonfind.chelsea.types;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum MemberChageAction {
+	JOINED("joined"),
+	LEFT("left");
+
+	private final String action;
+
+	public String getAction() {
+		return action;
+	}
 }
