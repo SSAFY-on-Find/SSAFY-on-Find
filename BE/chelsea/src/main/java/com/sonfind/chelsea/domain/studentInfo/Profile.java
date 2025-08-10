7b88cff2d2427b4607254dfa1b8359aa72423808
hyunji321 <77107811+hyunji321@users.noreply.github.com>
2025-08-11 01:27:52 +0900

    feat: 프로필을 저장하는 class 분리
    - savedFileName은 저장 시 파일 이름 저장
    - profileImageUrl은 저장된 파일 url 저장

---
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/domain/studentInfo/Profile.java
@@ -0,0 +1,19 @@
+package com.sonfind.chelsea.domain.studentInfo;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class Profile {
+
+	private String savedFileName;
+	private String profileImageUrl;
+
+}
