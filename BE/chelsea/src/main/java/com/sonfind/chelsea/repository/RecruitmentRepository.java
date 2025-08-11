c38849fdada8392706c3b51bcc3047292d4279ae
hyunji321 <77107811+hyunji321@users.noreply.github.com>
2025-08-11 17:17:20 +0900

    feat: 팀id 기준으로 포지션 목록 조회 구현

--- BE/chelsea/src/main/java/com/sonfind/chelsea/repository/RecruitmentRepository.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/repository/RecruitmentRepository.java
@@ -1,10 +1,17 @@
 package com.sonfind.chelsea.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.sonfind.chelsea.domain.teams.Recruitment;
+import com.sonfind.chelsea.global.domain.SubCode;
 
 @Repository
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
+
+	@Query("SELECT r.position FROM Recruitment r WHERE r.team.teamId = :teamId")
+	List<SubCode> findPositionByTeamId(Long teamId);
 }
