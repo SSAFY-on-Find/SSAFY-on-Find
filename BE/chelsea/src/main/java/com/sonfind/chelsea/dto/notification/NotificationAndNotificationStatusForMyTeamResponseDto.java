63ec834b0ea3630841ae2c816735c7c0ee819458
Jgone2 <jgoneit@gmail.com>
2025-08-13 14:07:37 +0900

    refactor: record null값인 필드 반환하지 않도록 수정

--- BE/chelsea/src/main/java/com/sonfind/chelsea/dto/notification/NotificationAndNotificationStatusForMyTeamResponseDto.java
+++ BE/chelsea/src/main/java/com/sonfind/chelsea/dto/notification/NotificationAndNotificationStatusForMyTeamResponseDto.java
@@ -9,7 +9,7 @@ public record NotificationAndNotificationStatusForMyTeamResponseDto(
 		String name,
 		String isMajor,
 		String track,
-		int majorCount,
-		int nonMajorCount
+		Integer majorCount,
+		Integer nonMajorCount
 ) {
 }
