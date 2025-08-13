ff19eaccfa6e5ff5e2bcac14e38aadc0c0c44a51
KIM SEONMIN <seonmiki@student.42seoul.kr>
2025-08-13 15:28:44 +0900

    팀 목록 알림 api 맞춰서 수정 및 sse 연결 로그 추가

--- FE/src/types/notification/index.ts
+++ FE/src/types/notification/index.ts
@@ -1,2 +1,4 @@
 export type { INotificationStatus } from "./INotiResponse"
 export type { INotification } from "./INotiResponse"
+export type { Status } from "./INotiResponse"
+export type { INotificationTeam } from "./INotiTeamResponse"
