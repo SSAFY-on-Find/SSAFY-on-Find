6756e54909e5925032640787390ed52bcfb87abd
JeongYeonKim <kimjeongyeon113@gmail.com>
2025-08-14 11:09:36 +0900

    fix: 대시보드와 교육생 studentInfo 클릭시 이동 오류 수정 & teamDetail 이미지 길쭉한거 수정

--- FE/src/components/molecules/TeamDetail.tsx
+++ FE/src/components/molecules/TeamDetail.tsx
@@ -116,7 +116,9 @@ function TeamDetail({
                       onClick={() => handleMemberClick(ele.studentId)}
                       className="hover:bg-main/10 flex flex-col items-center rounded-md p-1 px-[10px] hover:cursor-pointer"
                     >
-                      <UserImg name={ele.name} size={"m"} showTeamBadge={false} url={ele.profileImageUrl} />
+                      <div>
+                        <UserImg name={ele.name} size={"m"} showTeamBadge={false} url={ele.profileImageUrl} />
+                      </div>
                       <p className="mt-2 mb-[5px] text-sm">{ele.name}</p>
                       {ele.position && <PositionTag positionName={ele.position.subcodeName} />}
                     </div>
--- FE/src/pages/StudentDetailPage/index.tsx
+++ FE/src/pages/StudentDetailPage/index.tsx
@@ -112,7 +112,7 @@ export default function StudentDetailPage() {
           studentId={student?.studentId?.toString() ?? ""}
           imgUrl={profileImageUrl ?? ""}
           teamInfo={teamInfo}
-          variant="dashboard"
+          variant="detail"
         />
       </div>
       <div className="border-line flex w-full flex-col gap-7 rounded-lg border bg-white p-10">
